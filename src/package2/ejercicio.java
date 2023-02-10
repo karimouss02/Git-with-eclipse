c159735c8cd1d95ca8cb3ee7fa24e19bcaa9a086
Karim <kxrim02@gmail.com>
2023-02-10 16:27:27 +0100

    Second Modification

--- src/package2/ejercicio.java
+++ src/package2/ejercicio.java
@@ -5,6 +5,7 @@ public class ejercicio {
 	public static void main(String[]args) {
 		
 		System.out.println("This is a modification");
+		System.out.println("This is the second modification");
 	}
 	
 		
